032e013105cf7aa1f9ba07bc1dd4a534a571bc7a
Mootez <mootez.saad@medtech.tn>
2020-05-17 22:59:13 +0100

    Add variable arguments to User model class

--- app/src/main/java/tn/medtech/recruitmentsystemapp/api/models/User.java
+++ app/src/main/java/tn/medtech/recruitmentsystemapp/api/models/User.java
@@ -8,12 +8,13 @@ public class User {
     private String jwtToken;
     private String userId;
 
-    // For user registration
-    public User(String firstName, String lastName, String email, String password) {
+    // For user registration and getting back the user after login
+    public User(String firstName, String lastName, String email, String ...vars) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = password;
+        this.password = vars[0];
+        this.jwtToken = vars[1];
     }
     // For user login
     public User(String email, String password) {
