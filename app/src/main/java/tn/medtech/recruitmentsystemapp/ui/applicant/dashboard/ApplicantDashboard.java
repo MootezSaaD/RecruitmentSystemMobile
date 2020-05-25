2d185011d907f672687b47db4b529b5714357459
Mootez <mootez.saad@medtech.tn>
2020-05-25 09:44:09 +0100

    Add login redirection

---
+++ app/src/main/java/tn/medtech/recruitmentsystemapp/ui/applicant/dashboard/ApplicantDashboard.java
@@ -0,0 +1,16 @@
+package tn.medtech.recruitmentsystemapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tn.medtech.recruitmentsystemapp.R;
+
+public class ApplicantDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_applicant_dashboard);
+    }
+}
