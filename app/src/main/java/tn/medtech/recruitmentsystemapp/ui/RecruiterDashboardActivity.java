886ff7acabdeffd081a213c1b474105b8fb85ac9
Mootez <mootez.saad@medtech.tn>
2020-05-31 11:55:49 +0100

    Add sidebar menu

--- app/src/main/java/tn/medtech/recruitmentsystemapp/ui/RecruiterDashboardActivity.java
+++ app/src/main/java/tn/medtech/recruitmentsystemapp/ui/RecruiterDashboardActivity.java
@@ -1,20 +1,32 @@
 package tn.medtech.recruitmentsystemapp.ui;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.gson.Gson;
+
 import tn.medtech.recruitmentsystemapp.R;
+import tn.medtech.recruitmentsystemapp.api.models.Recruiter;
 
 public class RecruiterDashboardActivity extends AppCompatActivity {
     Button createJobButton;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    Recruiter recruiter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
         setContentView(R.layout.activity_recruiter_dashboard);
         createJobButton = findViewById(R.id.createJobButton);
 
@@ -25,5 +37,22 @@ public class RecruiterDashboardActivity extends AppCompatActivity {
                 startActivity(jobIntent);
             }
         });
+
+        toolbar = findViewById(R.id.recToolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.recDrawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
